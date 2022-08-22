d709cdbb068f8fe08a84e208cabb18aa5b79d324
Fraser Parlane <mail@fraserparlane.com>
2022-08-22 14:59:59 -0700

    Convert to PHP

---
+++ index.php
@@ -0,0 +1,59 @@
+
+<!-- Force page not to cache. -->
+<META HTTP-EQUIV="CACHE-CONTROL" CONTENT="NO-CACHE">
+<META HTTP-EQUIV="EXPIRES" CONTENT="Mon, 22 Jul 2002 11:12:01 GMT">
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Fraser Parlane, Ph.D.</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="is-preload">
+
+		<!-- Header -->
+			<header id="header">
+				<div class="inner">
+					<a href="#" class="image avatar"><img src="images/avatar.jpg" alt="" /></a>
+					<h1><strong>Fraser Parlane</strong>, Ph.D.<br /></h1>
+					<a href='&#109;a&#105;&#108;t&#111;&#58;%6Dail%4&#48;&#102;r%6&#49;se%&#55;&#50;%70&#97;&#114;%6&#67;a%&#54;Ee&#46;&#99;%6Fm'>mai&#108;&#64;&#102;raserparlane&#46;&#99;o&#109;</a><br />
+				</div>
+			</header>
+
+		<!-- Main -->
+			<div id="main">
+
+					<section>
+						<h1>Publications</h1>
+                        <?php echo '<p>Hello World</p>'; ?>
+                    </section>
+
+			</div>
+
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="inner">
+					<ul class="icons">
+						<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="#" class="icon brands fa-github"><span class="label">Github</span></a></li>
+						<li><a href="#" class="icon brands fa-dribbble"><span class="label">Dribbble</span></a></li>
+						<li><a href="#" class="icon solid fa-envelope"><span class="label">Email</span></a></li>
+					</ul>
+					<ul class="copyright">
+						<li>&copy; Untitled</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+					</ul>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.poptrox.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
