c0725e08099a2e229c7f8531da82b7654ffa5e95
Fraser Parlane <mail@fraserparlane.com>
2022-08-24 09:33:37 -0700

    Cleaned up PHP references.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 
 <!-- Force page not to cache. -->
-<!--<META HTTP-EQUIV="CACHE-CONTROL" CONTENT="NO-CACHE">-->
-<!--<META HTTP-EQUIV="EXPIRES" CONTENT="Mon, 22 Jul 2002 11:12:01 GMT">-->
+<META HTTP-EQUIV="CACHE-CONTROL" CONTENT="NO-CACHE">
+<META HTTP-EQUIV="EXPIRES" CONTENT="Mon, 22 Jul 2002 11:12:01 GMT">
 
 <!DOCTYPE HTML>
 <html>
@@ -11,65 +11,53 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<link rel="stylesheet" href="assets/css/main.css" />
         <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
-
-        <!-- Preloads -->
-	</head>
+    </head>
     <body class="is-preload">
-        <link rel="prefetch" as="image" href="images/research/covid-hover.png">
 
-		<!-- Header -->
-			<header id="header">
-				<div class="inner">
-					<a href="#" class="image avatar"><img src="images/avatar.jpg" alt="" /></a>
-					<h1><strong>Fraser Parlane</strong>, Ph.D.<br /></h1>
-					<a href='&#109;a&#105;&#108;t&#111;&#58;%6Dail%4&#48;&#102;r%6&#49;se%&#55;&#50;%70&#97;&#114;%6&#67;a%&#54;Ee&#46;&#99;%6Fm'>mai&#108;&#64;&#102;raserparlane&#46;&#99;o&#109;</a><br />
-				</div>
-			</header>
+        <!-- Header -->
+        <header id="header">
+            <div class="inner">
+                <a href="#" class="image avatar"><img src="images/avatar.jpg" alt="" /></a>
+                <h1><strong>Fraser Parlane</strong>, Ph.D.<br /></h1>
+                <a href='&#109;a&#105;&#108;t&#111;&#58;%6Dail%4&#48;&#102;r%6&#49;se%&#55;&#50;%70&#97;&#114;%6&#67;a%&#54;Ee&#46;&#99;%6Fm'>mai&#108;&#64;&#102;raserparlane&#46;&#99;o&#109;</a><br />
+            </div>
+        </header>
 
-		<!-- Main -->
-			<div id="main">
+        <!-- Main -->
+        <div id="main">
+                <section>
 
-					<section>
+                    <h1 id="education">Education</h1>
+                    <?php include 'php/education.php'; ?>
 
-                        <h1>Education</h1>
-                        <?php include 'php/education.php'; ?>
+                    <h1 id="research">Research</h1>
+                    <?php include 'php/research.php'; ?>
 
-                        <h1>Research</h1>
-                        <?php include 'php/research.php'; ?>
+                    <h1 id="awards">Awards and Honors</h1>
+                    <?php include 'php/awards.php'; ?>
 
-                        <h1>Awards and Honors</h1>
-                        <table class="years">
-                            <?php include 'php/awards.php'; ?>
-                        </table>
+                    <h1 id="publications">Publications</h1>
+                    <?php include 'php/publications.php'; ?>
 
+                </section>
+        </div>
 
-						<h1>Publications</h1>
-						A (*) denotes where two authors contributed equally.<br /><br />
-						<table class="references">
-                            <?php include 'php/publications.php'; ?>
-                        </table>
+        <!-- Footer -->
+        <footer id="footer">
+            <div class="inner">
+                <ul class="copyright">
+                    <li>&copy; Fraser Parlane</li>
+                </ul>
+            </div>
+        </footer>
 
-
-                    </section>
-
-			</div>
-
-		<!-- Footer -->
-			<footer id="footer">
-				<div class="inner">
-					<ul class="copyright">
-						<li>&copy; Fraser Parlane</li>
-					</ul>
-				</div>
-			</footer>
-
-		<!-- Scripts -->
-			<script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/jquery.poptrox.min.js"></script>
-			<script src="assets/js/browser.min.js"></script>
-			<script src="assets/js/breakpoints.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<script src="assets/js/main.js"></script>
+        <!-- Scripts -->
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/js/jquery.poptrox.min.js"></script>
+        <script src="assets/js/browser.min.js"></script>
+        <script src="assets/js/breakpoints.min.js"></script>
+        <script src="assets/js/util.js"></script>
+        <script src="assets/js/main.js"></script>
 
 	</body>
 </html>
--- php/research.php
+++ php/research.php
@@ -2,12 +2,56 @@
     <tr>
         <td>
             <div class="research-box">
-<!--                <img class="research-image-blue covid-hover" src="images/research/covid-hover.png">-->
+                <img class="research-image research-image-blue" src="images/research/sdl-hover.png">
+                <img class="research-image research-image-grey" src="images/research/sdl.png">
+            </div>
+        </td>
+        <td><b>I built the world’s first self-driving laboratory for thin-film materials.</b>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam venenatis molestie ante in dapibus. Curabitur viverra aliquet purus, sit amet feugiat magna interdum id. Vivamus id porttitor lacus. Vivamus commodo nulla ut augue sagittis sodales. Proin et convallis augue.
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <div class="research-box">
+                <img class="research-image research-image-blue" src="images/research/arc-hover.png">
+                <img class="research-image research-image-grey" src="images/research/arc.png">
+            </div>
+        </td>
+        <td><b>[Google]</b>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam venenatis molestie ante in dapibus. Curabitur viverra aliquet purus, sit amet feugiat magna interdum id. Vivamus id porttitor lacus. Vivamus commodo nulla ut augue sagittis sodales. Proin et convallis augue.
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <div class="research-box">
                 <img class="research-image research-image-blue" src="images/research/covid-hover.png">
                 <img class="research-image research-image-grey" src="images/research/covid.png">
             </div>
         </td>
-        <td><b>I built the world’s first self-driving laboratory for thin-film materials.</b>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam venenatis molestie ante in dapibus. Curabitur viverra aliquet purus, sit amet feugiat magna interdum id. Vivamus id porttitor lacus. Vivamus commodo nulla ut augue sagittis sodales. Proin et convallis augue.</td>
+        <td><b>[CovidBox]</b>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam venenatis molestie ante in dapibus. Curabitur viverra aliquet purus, sit amet feugiat magna interdum id. Vivamus id porttitor lacus. Vivamus commodo nulla ut augue sagittis sodales. Proin et convallis augue.
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <div class="research-box">
+                <img class="research-image research-image-blue" src="images/research/xb-hover.png">
+                <img class="research-image research-image-grey" src="images/research/xb.png">
+            </div>
+        </td>
+        <td><b>[Halogen Bonding]</b>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam venenatis molestie ante in dapibus. Curabitur viverra aliquet purus, sit amet feugiat magna interdum id. Vivamus id porttitor lacus. Vivamus commodo nulla ut augue sagittis sodales. Proin et convallis augue.
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <div class="research-box">
+                <img class="research-image research-image-blue" src="images/research/mri-hover.png">
+                <img class="research-image research-image-grey" src="images/research/mri.png">
+            </div>
+        </td>
+        <td><b>[Fluorine / MRI]</b>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam venenatis molestie ante in dapibus. Curabitur viverra aliquet purus, sit amet feugiat magna interdum id. Vivamus id porttitor lacus. Vivamus commodo nulla ut augue sagittis sodales. Proin et convallis augue.
+        </td>
     </tr>
 </table>
