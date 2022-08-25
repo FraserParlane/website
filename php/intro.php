873aee8e8d17e7b9ec32f9df949578ce488d6d6f
Fraser Parlane <mail@fraserparlane.com>
2022-08-25 13:46:15 -0700

    Email now obfuscated via a PHP request.

--- php/intro.php
+++ php/intro.php
@@ -1,2 +1,4 @@
 My research aims to accelerate the discovery and optimization of advanced clean energy materials by developing self-driving laboratories that combine flexible automation with machine learning and computational screening.<br />
-<a href=""><span class="icon solid fa-mobile-alt">&nbsp;google scholar, github, map location</span></a>
+<!--<span class="icon solid fa-mobile-alt"></span><a href=""><span class="icon solid fa-mobile-alt">&nbsp;google scholar</span></a>-->
+<!--<a href=""><span class="icon solid fa-email-alt">email</span></a>-->
+<a href="php/mailto.php">email</a>
---
+++ php/mailto.php
@@ -0,0 +1,3 @@
+<?php
+    header("Location: mailto:mail@fraserparlane.com");
+?>
