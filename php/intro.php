d04637f22498f2b3fbe2a70f9a27c655c99d0c55
Fraser Parlane <mail@fraserparlane.com>
2022-08-25 14:17:20 -0700

    Actions list collapses sooner.

--- php/intro.php
+++ php/intro.php
@@ -1,4 +1,11 @@
-My research aims to accelerate the discovery and optimization of advanced clean energy materials by developing self-driving laboratories that combine flexible automation with machine learning and computational screening.<br />
-<!--<span class="icon solid fa-mobile-alt"></span><a href=""><span class="icon solid fa-mobile-alt">&nbsp;google scholar</span></a>-->
-<!--<a href=""><span class="icon solid fa-email-alt">email</span></a>-->
-<a href="php/mailto.php">email</a>
+<p class="intro-text">
+    My research aims to accelerate the discovery and optimization of advanced clean energy materials by developing self-driving laboratories that combine flexible automation with machine learning and computational screening.
+</p>
+
+
+<ul class="actions">
+    <li><a href="php/mailto.php" class="button" target="_blank"><span class="icon solid fa-envelope"></span>Email</a></li>
+    <li><a href="https://scholar.google.com/citations?user=iNFigAoAAAAJ&hl=en" class="button" target="_blank"><span class="icon solid fa-graduation-cap"></span>Google Scholar</a></li>
+    <li><a href="https://github.com/FraserParlane" class="button" target="_blank"><span class="icon solid fa-code-branch"></span>GitHub</a></li>
+    <li><a href="https://github.com/FraserParlane/thesis" class="button" target="_blank"><span class="icon solid fa-book"></span>Thesis</a></li>
+</ul>
