a2901d6f5cd7c79963c44f45a0a96be46edd2db5
Fraser Parlane <mail@fraserparlane.com>
2022-08-25 15:46:01 -0700

    Now links to defence website.

--- php/intro.php
+++ php/intro.php
@@ -1,11 +1,9 @@
-<p class="intro-text">
-    My research aims to accelerate the discovery and optimization of advanced clean energy materials by developing self-driving laboratories that combine flexible automation with machine learning and computational screening.
-</p>
+<h2 class="intro-text">My research aims to accelerate the discovery and optimization of advanced clean energy materials by developing self-driving laboratories that combine flexible automation with machine learning and computational screening.</h2>
 
 
 <ul class="actions actions-top">
     <li><a href="php/mailto.php" class="button" target="_blank"><span class="icon solid fa-envelope"></span>Email</a></li>
     <li><a href="https://scholar.google.com/citations?user=iNFigAoAAAAJ&hl=en" class="button" target="_blank"><span class="icon solid fa-graduation-cap"></span>Google Scholar</a></li>
     <li><a href="https://github.com/FraserParlane" class="button" target="_blank"><span class="icon solid fa-code-branch"></span>GitHub</a></li>
-    <li><a href="https://github.com/FraserParlane/thesis" class="button" target="_blank"><span class="icon solid fa-book"></span>Thesis</a></li>
+    <li><a href="http://www.defence.parlane.ca/" class="button" target="_blank"><span class="icon solid fa-book"></span>Thesis</a></li>
 </ul>
