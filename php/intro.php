7e7325b7da48f863cce22daf618ac91d0b1182c3
Fraser Parlane <mail@fraserparlane.com>
2022-08-25 14:21:02 -0700

    CSS references improved, spacing improved.

--- php/intro.php
+++ php/intro.php
@@ -3,7 +3,7 @@
 </p>
 
 
-<ul class="actions">
+<ul class="actions actions-top">
     <li><a href="php/mailto.php" class="button" target="_blank"><span class="icon solid fa-envelope"></span>Email</a></li>
     <li><a href="https://scholar.google.com/citations?user=iNFigAoAAAAJ&hl=en" class="button" target="_blank"><span class="icon solid fa-graduation-cap"></span>Google Scholar</a></li>
     <li><a href="https://github.com/FraserParlane" class="button" target="_blank"><span class="icon solid fa-code-branch"></span>GitHub</a></li>
