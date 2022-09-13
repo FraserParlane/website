d985bc751c5516d1bbf371ac86634c0563f7eab6
Fraser Parlane <mail@fraserparlane.com>
2022-09-12 18:56:17 -0700

    Featured articles moved up.

--- index.php
+++ index.php
@@ -28,6 +28,7 @@
 
                 <?php include 'php/intro.php'; ?>
 
+                <h1 id="highlight">Featured in</h1>
                 <?php include 'php/highlights.php'; ?>
 
                 <h1 id="experience">Experience</h1>
--- php/highlights.php
+++ php/highlights.php
@@ -1,1 +1,19 @@
-My research has been featured in:
+<p class="highlight-text">
+    My 5 first-author and 11 co-author publications have been cited more than >600 times (H-index 9).
+</p>
+
+<a href="https://www.nature.com/articles/d41586-019-01683-9" target="_blank" class="highlight-link">
+    <img src="images/articles/nature.webp" class="highlight-image">
+</a>
+<a href="https://www.science.org/content/article/ai-driven-robots-are-making-new-materials-improving-solar-cells-and-other-technologies" target="_blank" class="highlight-link">
+    <img src="images/articles/science.webp" class="highlight-image">
+</a>
+<a href="https://www.forbes.com/sites/stevensalzberg/2019/06/10/google-ran-a-secret-experiment-to-search-for-cold-fusion-did-they-find-it" target="_blank" class="highlight-link">
+    <img src="images/articles/forbes.webp" class="highlight-image">
+</a>
+<a href="https://www.nationalgeographic.com/science/article/cold-fusion-remains-elusive-these-scientists-may-revive-quest" target="_blank" class="highlight-link">
+    <img src="images/articles/national-geographic.webp" class="highlight-image">
+</a>
+<a href="https://youtu.be/Ic9sFz-4dn0" target="_blank" class="highlight-link">
+    <img src="images/articles/cbc.webp" class="highlight-image">
+</a>
