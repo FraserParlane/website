3d1943675b03c741d4b5a59416af72160e9b94f0
Fraser Parlane <mail@fraserparlane.com>
2022-09-12 17:12:16 -0700

    Fonts updated, intro complete.

--- php/header.php
+++ php/header.php
@@ -1,6 +1,5 @@
 <div class="inner">
     <a href="#body" class="image avatar"><img src="images/avatar.webp" alt="" /></a>
-    <h1><strong>Fraser Parlane</strong>, Ph.D.</h1>
     <ul class="menu-links">
         <li><a href="#education">Education</a></li>
         <li><a href="#research">Research</a></li>
--- php/intro.php
+++ php/intro.php
@@ -1,4 +1,9 @@
-<p class="intro-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse finibus sit amet velit nec pretium. Aenean quis efficitur nibh. In venenatis eu felis congue tincidunt. Mauris in urna semper, varius odio vitae, venenatis odio.</p>
+<span class="my-name">Fraser <b>Parlane </b></span><span class="my-name-suffix">PhD</span><br />
+<span class="my-name-slug">Data Scientist, Chemist, Project Manager</span>
+
+<p class="intro-text">
+    <a class="intro-text">I am an expert in <a href="https://youtu.be/2h9-9PxwbQM">data-driven storytelling</a>, hypothesis-guided experimentation, and computational simulations. Over the past four years, I led an $8M project to build the world’s first <a href="https://www.nature.com/articles/s41467-022-28580-6">self-driving laboratory</a> to discover thin-film materials autonomously. Prior to that, I processed and visualized <a href="https://www.nature.com/articles/s41586-019-1256-6">advanced nuclear fusion</a> data at Google and <a href="https://www.nature.com/articles/s41467-017-01726-7">redefined a chemical bond</a> at Stanford.
+</p>
 
 <ul class="actions actions-top">
     <li><a href="php/mailto.php" class="button" target="_blank"><span class="icon solid fa-envelope"></span>Email</a></li>
