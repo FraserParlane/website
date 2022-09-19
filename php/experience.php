3a4e80749569815b24839f1c03766d6be875dd3b
Fraser Parlane <mail@fraserparlane.com>
2022-09-18 19:53:01 -0700

    Experience graphics completed.

--- php/experience.php
+++ php/experience.php
@@ -6,127 +6,75 @@
             <img class="research-image" src="images/research/sdl.webp">
         </td>
         <td>
-            <p>I cofounded and led a program of >30 engineers and scientists that used <a href="https://www.nature.com/articles/s41563-021-01156-3" target="_blank">flexible automation</a> to deploy <a href="https://youtu.be/hFBMSkihjHE">self-driving laboratories</a> that automate materials discovery. As the lead data scientist, I implemented <a href="https://www.nature.com/articles/s41467-022-28580-6" target="_blank">multi-objective</a> <a href="https://www.science.org/doi/10.1126/sciadv.aaz8867" target="_blank">Bayesian optimization</a> algorithms, performed multithreaded simulations, developed data pipelines and dashboards, designed automated experiments, and designed and integrated custom spectrometers. The project resulted in <a href="https://scholar.google.com/citations?user=iNFigAoAAAAJ&hl=en" target="_blank">8 publications</a>, >200 citations, and >$1.5M in follow-on funding.</p>
+            <p>
+                I cofounded and led a program of >30 engineers and scientists that used <a href="https://www.nature.com/articles/s41563-021-01156-3" target="_blank">flexible automation</a> to deploy <a href="https://youtu.be/hFBMSkihjHE">self-driving laboratories</a> that automate materials discovery. As the lead data scientist, I implemented <a href="https://www.nature.com/articles/s41467-022-28580-6" target="_blank">multi-objective</a> <a href="https://www.science.org/doi/10.1126/sciadv.aaz8867" target="_blank">Bayesian optimization</a> algorithms, performed multithreaded simulations, developed data pipelines and dashboards, designed automated experiments, and designed and integrated custom spectrometers. The project resulted in <a href="https://scholar.google.com/citations?user=iNFigAoAAAAJ&hl=en" target="_blank">8 publications</a>, >200 citations, and >$1.5M in follow-on funding.
+            </p>
         </td>
     </tr>
 </table>
-
-<h3 class="experience-heading">I built the world’s first self-driving laboratory for thin films<span class="heading-year"> | 2018-2022</span></h3>
-<p>I cofounded and led a program of >30 engineers and scientists that used <a href="https://www.nature.com/articles/s41563-021-01156-3" target="_blank">flexible automation</a> to deploy <a href="https://youtu.be/hFBMSkihjHE">self-driving laboratories</a> that automate materials discovery. As the lead data scientist, I implemented <a href="https://www.nature.com/articles/s41467-022-28580-6" target="_blank">multi-objective</a> <a href="https://www.science.org/doi/10.1126/sciadv.aaz8867" target="_blank">Bayesian optimization</a> algorithms, performed multithreaded simulations, developed data pipelines and dashboards, designed automated experiments, and designed and integrated custom spectrometers. The project resulted in <a href="https://scholar.google.com/citations?user=iNFigAoAAAAJ&hl=en" target="_blank">8 publications</a>, >200 citations, and >$1.5M in follow-on funding.</p>
 
 <h3 class="experience-heading">I processed and visualized advanced nuclear fusion data at Google<span class="heading-year"> | 2017</span></h3>
-<p>As a technical project manager and software engineer intern, I created a dashboard that ingested advanced calorimetry data. The dashboard enabled 30 collaborators to make on-the-fly experimental decisions. I <a href="https://github.com/calorimetry/CSID" target="_blank">open-sourced</a> system identification software used to model the <a href="https://www.nature.com/articles/s41586-019-1256-6">fusion reactors</a>.</p>
-
-<h3 class="experience-heading">I redefined a chemical bond<span class="heading-year"> | 2016-2020</span></h3>
-<p>I directly observed the <a href="https://onlinelibrary.wiley.com/doi/abs/10.1002/anie.201510641" target="_blank">interfacial chemistry</a> of a solution-processed solar cell for the first time and improved the cell performance by <a href="https://pubs.acs.org/doi/10.1021/jacs.6b06288" target="_blank">25%</a>. Prior to my work, a halogen bond was described as only containing sigma symmetry. I showed the chemistry community for the first time that the bond also <a href="https://www.nature.com/articles/s41467-020-17122-7" target="_blank">contains pi covalency</a> with <a href="https://www.nature.com/articles/s41467-017-01726-7" target="_blank">non-conventional synchrotron techniques</a>.</p>
-
-<h3 class="experience-heading">I distributed an enclosure to protect healthcare workers from COVID-19<span class="heading-year"> | 2020</span></h3>
-<p>Healthcare staff risk infection when intubating COVID-19 patients. I distributed an <a href="https://covidbox.ca/" target="_blank">open-source enclosure</a> to protect staff who are performing procedures on infectious patients. The enclosure was rapidly prototyped in <a href="https://www.youtube.com/watch?v=4mNC8dMF-p4">partnership with healthcare staff</a>, has been adopted in more than 5 countries, and is used on actual patients.</p>
-
-<h3 class="experience-heading">I taught chemistry lectures and laboratories at UBC and TWU<span class="heading-year"> | 2015-2016, 2020</span></h3>
-<p>As a laboratory lecturer and teaching assistant, I Instructed >600 students and mentored 28 teaching assistants. I received >95% favorable <a href="http://demo.parlane.ca/documents/Fraser-Parlane-TA-evaluations.pdf" target="_blank">course reviews</a>.</p>
-
-<h3 class="experience-heading">I developed novel fluorine polymers for use as MRI contrast reagents<span class="heading-year"> | 2014</span></h3>
-<p>I built self-assembling block copolymers for use as MRI contrast reagents at ENSCM, France. I used RAFT polymerization to control the size distribution of fluorine- and phosphorus-containing polymers for use as tailored therapeutics.</p>
-
-<h3 class="experience-heading">I cofounded a tutoring company<span class="heading-year"> | 2015-2017</span></h3>
-<p>I cofounded a tutoring company with 5 tutors that mentored secondary and post-secondary students in STEM subjects. Our services were always fully booked.</p>
-
-<h3 class="experience-heading">I built catalysts for pharmaceutical and polymer synthesis<span class="heading-year"> | 2010-2015</span></h3>
-<p>I build <a href="https://www.sciencedirect.com/science/article/abs/pii/S002016931400019X" target="_blank">asymmetric catalysts</a> for advanced pharmaceutical synthesis and <a href="https://www.sciencedirect.com/science/article/abs/pii/S0020169317301937" target="_blank">polymerization</a>. I was awarded three NSERC research grants to perform this work.</p>
-
-
-
-<!--
-<table class="research-table table-collapse">
+<table class="image-table">
     <tr>
         <td>
-            <div class="research-box">
-                <img class="research-image research-image-blue" src="images/research/sdl-hover.webp">
-                <img class="research-image research-image-grey" src="images/research/sdl-new.webp">
-            </div>
+            <img class="research-image research-image-grey" src="images/research/berkley-grey.webp">
+            <img class="research-image" src="images/research/berkley.webp">
         </td>
-        <td><h3>I built the world’s first self-driving laboratory for thin-film materials.</h3>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam venenatis molestie ante in dapibus. Curabitur viverra aliquet purus, sit amet feugiat magna interdum id. Vivamus id porttitor lacus. Vivamus commodo nulla ut augue sagittis sodales.
-            <a href="https://www.nature.com/articles/s41467-022-28580-6" target="_blank">Nat. Comm. 2022</a>,
-            <a href="https://www.degruyter.com/document/doi/10.1515/9783110738087-004/html" target="_blank">De Gruyter 2022</a>,
-            <a href="https://www.nature.com/articles/s41563-021-01156-3" target="_blank">Nat. Mat. 2021</a>,
-            <a href="https://www.nature.com/articles/s41524-020-00380-w" target="_blank">Npj Comput. Mater. 2020</a>,
-            <a href="https://www.science.org/doi/10.1126/sciadv.aaz8867" target="_blank">Sci. Adv. 2020</a>,
-            <a href="https://www.science.org/content/article/ai-driven-robots-are-making-new-materials-improving-solar-cells-and-other-technologies" target="_blank">Science News 2019</a>,
-            <a href="https://www.youtube.com/watch?v=hFBMSkihjHE" target="_blank">YouTube</a>.
-        </td>
-    </tr>
-    <tr>
         <td>
-            <div class="research-box">
-                <img class="research-image research-image-blue" src="images/research/arc-hover.webp">
-                <img class="research-image research-image-grey" src="images/research/arc.webp">
-            </div>
+            <p>
+                As a technical project manager and software engineer intern, I created a dashboard that ingested advanced calorimetry data. The dashboard enabled 30 collaborators to make on-the-fly experimental decisions. I <a href="https://github.com/calorimetry/CSID" target="_blank">open-sourced</a> system identification software used to model the <a href="https://www.nature.com/articles/s41586-019-1256-6">fusion reactors</a>.
+            </p>
         </td>
-        <td><h3>[data scientist for Google to work on advanced nuclear fusion reactors.]</h3>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam venenatis molestie ante in dapibus. Curabitur viverra aliquet purus, sit amet feugiat magna interdum id. Vivamus id porttitor lacus. Vivamus commodo nulla ut augue sagittis sodales.
-            <a href="https://www.nature.com/articles/s41467-022-28580-6" target="_blank">Nat. Comm. 2022</a>,
-            <a href="https://www.degruyter.com/document/doi/10.1515/9783110738087-004/html" target="_blank">De Gruyter 2022</a>,
-            <a href="https://www.nature.com/articles/s41563-021-01156-3" target="_blank">Nat. Mat. 2021</a>,
-            <a href="https://www.nature.com/articles/s41524-020-00380-w" target="_blank">Npj Comput. Mater. 2020</a>,
-            <a href="https://www.science.org/doi/10.1126/sciadv.aaz8867" target="_blank">Sci. Adv. 2020</a>,
-            <a href="https://www.science.org/content/article/ai-driven-robots-are-making-new-materials-improving-solar-cells-and-other-technologies" target="_blank">Science News 2019</a>,
-            <a href="https://www.youtube.com/watch?v=hFBMSkihjHE" target="_blank">YouTube</a>.            <a href="" target="_blank"> 202</a>,
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <div class="research-box">
-                <img class="research-image research-image-blue" src="images/research/covid-hover.webp">
-                <img class="research-image research-image-grey" src="images/research/covid.webp">
-            </div>
-        </td>
-        <td><h3>[CovidBox]</h3>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam venenatis molestie ante in dapibus. Curabitur viverra aliquet purus, sit amet feugiat magna interdum id. Vivamus id porttitor lacus. Vivamus commodo nulla ut augue sagittis sodales.
-            <a href="https://www.nature.com/articles/s41467-022-28580-6" target="_blank">Nat. Comm. 2022</a>,
-            <a href="https://www.degruyter.com/document/doi/10.1515/9783110738087-004/html" target="_blank">De Gruyter 2022</a>,
-            <a href="https://www.nature.com/articles/s41563-021-01156-3" target="_blank">Nat. Mat. 2021</a>,
-            <a href="https://www.nature.com/articles/s41524-020-00380-w" target="_blank">Npj Comput. Mater. 2020</a>,
-            <a href="https://www.science.org/doi/10.1126/sciadv.aaz8867" target="_blank">Sci. Adv. 2020</a>,
-            <a href="https://www.science.org/content/article/ai-driven-robots-are-making-new-materials-improving-solar-cells-and-other-technologies" target="_blank">Science News 2019</a>,
-            <a href="https://www.youtube.com/watch?v=hFBMSkihjHE" target="_blank">YouTube</a>.            <a href="" target="_blank"> 202</a>,
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <div class="research-box">
-                <img class="research-image research-image-blue" src="images/research/xb-hover.webp">
-                <img class="research-image research-image-grey" src="images/research/xb.webp">
-            </div>
-        </td>
-        <td><h3>[Halogen Bonding]</h3>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam venenatis molestie ante in dapibus. Curabitur viverra aliquet purus, sit amet feugiat magna interdum id. Vivamus id porttitor lacus. Vivamus commodo nulla ut augue sagittis sodales.
-            <a href="https://www.nature.com/articles/s41467-022-28580-6" target="_blank">Nat. Comm. 2022</a>,
-            <a href="https://www.degruyter.com/document/doi/10.1515/9783110738087-004/html" target="_blank">De Gruyter 2022</a>,
-            <a href="https://www.nature.com/articles/s41563-021-01156-3" target="_blank">Nat. Mat. 2021</a>,
-            <a href="https://www.nature.com/articles/s41524-020-00380-w" target="_blank">Npj Comput. Mater. 2020</a>,
-            <a href="https://www.science.org/doi/10.1126/sciadv.aaz8867" target="_blank">Sci. Adv. 2020</a>,
-            <a href="https://www.science.org/content/article/ai-driven-robots-are-making-new-materials-improving-solar-cells-and-other-technologies" target="_blank">Science News 2019</a>,
-            <a href="https://www.youtube.com/watch?v=hFBMSkihjHE" target="_blank">YouTube</a>.        </td>
-    </tr>
-    <tr>
-        <td>
-            <div class="research-box">
-                <img class="research-image research-image-blue" src="images/research/mri-hover.webp">
-                <img class="research-image research-image-grey" src="images/research/mri.webp">
-            </div>
-        </td>
-        <td><h3>[Fluorine / MRI]</h3>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam venenatis molestie ante in dapibus. Curabitur viverra aliquet purus, sit amet feugiat magna interdum id. Vivamus id porttitor lacus. Vivamus commodo nulla ut augue sagittis sodales.
-            <a href="https://www.nature.com/articles/s41467-022-28580-6" target="_blank">Nat. Comm. 2022</a>,
-            <a href="https://www.degruyter.com/document/doi/10.1515/9783110738087-004/html" target="_blank">De Gruyter 2022</a>,
-            <a href="https://www.nature.com/articles/s41563-021-01156-3" target="_blank">Nat. Mat. 2021</a>,
-            <a href="https://www.nature.com/articles/s41524-020-00380-w" target="_blank">Npj Comput. Mater. 2020</a>,
-            <a href="https://www.science.org/doi/10.1126/sciadv.aaz8867" target="_blank">Sci. Adv. 2020</a>,
-            <a href="https://www.science.org/content/article/ai-driven-robots-are-making-new-materials-improving-solar-cells-and-other-technologies" target="_blank">Science News 2019</a>,
-            <a href="https://www.youtube.com/watch?v=hFBMSkihjHE" target="_blank">YouTube</a>.        </td>
     </tr>
 </table>
 
--->
+<h3 class="experience-heading">I redefined a chemical bond<span class="heading-year"> | 2016-2020</span></h3>
+<table class="image-table">
+    <tr>
+        <td>
+            <img class="research-image research-image-grey" src="images/research/ssrl-grey.webp">
+            <img class="research-image" src="images/research/ssrl.webp">
+        </td>
+        <td>
+            <p>
+                I directly observed the <a href="https://onlinelibrary.wiley.com/doi/abs/10.1002/anie.201510641" target="_blank">interfacial chemistry</a> of a solution-processed solar cell for the first time and improved the cell performance by <a href="https://pubs.acs.org/doi/10.1021/jacs.6b06288" target="_blank">25%</a>. Prior to my work, a halogen bond was described as only containing sigma symmetry. I showed the chemistry community for the first time that the bond also <a href="https://www.nature.com/articles/s41467-020-17122-7" target="_blank">contains pi covalency</a> with <a href="https://www.nature.com/articles/s41467-017-01726-7" target="_blank">non-conventional synchrotron techniques</a>.
+            </p>
+        </td>
+    </tr>
+</table>
+
+<h3 class="experience-heading">I distributed an enclosure to protect healthcare workers from COVID-19<span class="heading-year"> | 2020</span></h3>
+<table class="image-table">
+    <tr>
+        <td>
+            <img class="research-image research-image-grey" src="images/research/covid-grey.webp">
+            <img class="research-image" src="images/research/covid.webp">
+        </td>
+        <td>
+            <p>
+                Healthcare staff risk infection when intubating COVID-19 patients. I distributed an <a href="https://covidbox.ca/" target="_blank">open-source enclosure</a> to protect staff who are performing procedures on infectious patients. The enclosure was rapidly prototyped in <a href="https://www.youtube.com/watch?v=4mNC8dMF-p4">partnership with healthcare staff</a>, has been adopted in more than 5 countries, and is used on actual patients.
+            </p>
+        </td>
+    </tr>
+</table>
+
+
+<h3 class="experience-heading">I taught chemistry lectures and laboratories at UBC and TWU<span class="heading-year"> | 2015-2016, 2020</span></h3>
+<p>
+    As a laboratory lecturer and teaching assistant, I Instructed >600 students and mentored 28 teaching assistants. I received >95% favorable <a href="http://demo.parlane.ca/documents/Fraser-Parlane-TA-evaluations.pdf" target="_blank">course reviews</a>.
+</p>
+
+<h3 class="experience-heading">I developed novel fluorine polymers for use as MRI contrast reagents<span class="heading-year"> | 2014</span></h3>
+<p>
+    I built self-assembling block copolymers for use as MRI contrast reagents at ENSCM, France. I used RAFT polymerization to control the size distribution of fluorine- and phosphorus-containing polymers for use as tailored therapeutics.
+</p>
+
+<h3 class="experience-heading">I cofounded a tutoring company<span class="heading-year"> | 2015-2017</span></h3>
+<p>
+    I cofounded a tutoring company with 5 tutors that mentored secondary and post-secondary students in STEM subjects. Our services were always fully booked.
+</p>
+
+<h3 class="experience-heading">I built catalysts for pharmaceutical and polymer synthesis<span class="heading-year"> | 2010-2015</span></h3>
+<p>
+    I build <a href="https://www.sciencedirect.com/science/article/abs/pii/S002016931400019X" target="_blank">asymmetric catalysts</a> for advanced pharmaceutical synthesis and <a href="https://www.sciencedirect.com/science/article/abs/pii/S0020169317301937" target="_blank">polymerization</a>. I was awarded three NSERC research grants to perform this work.
+</p>
