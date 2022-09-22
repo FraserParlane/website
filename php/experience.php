b804c20cb094d7688b87b8e0101bbd788fe01d03
Fraser Parlane <mail@fraserparlane.com>
2022-09-21 21:03:08 -0700

    Experience text updated.

--- php/experience.php
+++ php/experience.php
@@ -7,13 +7,13 @@
         </td>
         <td>
             <p>
-                I cofounded and led a program of >30 engineers and scientists that used <a href="https://www.nature.com/articles/s41563-021-01156-3" target="_blank">flexible automation</a> to deploy <a href="https://youtu.be/hFBMSkihjHE">self-driving laboratories</a> that automate materials discovery. As the lead data scientist, I implemented <a href="https://www.nature.com/articles/s41467-022-28580-6" target="_blank">multi-objective</a> <a href="https://www.science.org/doi/10.1126/sciadv.aaz8867" target="_blank">Bayesian optimization</a> algorithms, performed multithreaded simulations, developed data pipelines and dashboards, designed automated experiments, and designed and integrated custom spectrometers. The project resulted in <a href="https://scholar.google.com/citations?user=iNFigAoAAAAJ&hl=en" target="_blank">8 publications</a>, >200 citations, and >$1.5M in follow-on funding.
+                I cofounded and led a program of >30 engineers and scientists that deployed <a href="https://youtu.be/hFBMSkihjHE">autonomous robotic laboratories</a>. My team used <a href="https://www.nature.com/articles/s41563-021-01156-3" target="_blank">flexible automation</a> to build self-driving labs that automate materials discovery. As lead data scientist, I implemented <a href="https://www.nature.com/articles/s41467-022-28580-6" target="_blank">multi-objective</a> <a href="https://www.science.org/doi/10.1126/sciadv.aaz8867" target="_blank">Bayesian optimization</a> algorithms, performed multithreaded simulations, developed pipelines and dashboards, designed automated experiments, and created custom spectrometers. The project resulted in <a href="https://scholar.google.com/citations?user=iNFigAoAAAAJ&hl=en" target="_blank">8 publications</a>, >200 citations, and >$1.5M in follow-on funding.
             </p>
         </td>
     </tr>
 </table>
 
-<h3 class="experience-heading">I processed and visualized advanced nuclear fusion data at Google<span class="heading-year"> | 2017</span></h3>
+<h3 class="experience-heading">I processed and visualized data from a nuclear fusion reactor at Google<span class="heading-year"> | 2017</span></h3>
 <table class="image-table">
     <tr>
         <td>
@@ -22,7 +22,7 @@
         </td>
         <td>
             <p>
-                As a technical project manager and software engineer intern, I created a dashboard that ingested advanced calorimetry data. The dashboard enabled 30 collaborators to make on-the-fly experimental decisions. I <a href="https://github.com/calorimetry/CSID" target="_blank">open-sourced</a> system identification software used to model the <a href="https://www.nature.com/articles/s41586-019-1256-6">fusion reactors</a>.
+                As a Technical Program Manager and Software Engineer intern, I created a dashboard that ingested calorimetry data from <a href="https://www.nature.com/articles/s41586-019-1256-6">fusion reactors</a>. The dashboard enabled 30 collaborators to make on-the-fly experimental decisions. I <a href="https://github.com/calorimetry/CSID" target="_blank">open-sourced</a> the system identification software used to model the reactors.
             </p>
         </td>
     </tr>
@@ -37,13 +37,13 @@
         </td>
         <td>
             <p>
-                I directly observed the <a href="https://onlinelibrary.wiley.com/doi/abs/10.1002/anie.201510641" target="_blank">interfacial chemistry</a> of a solution-processed solar cell for the first time and improved the cell performance by <a href="https://pubs.acs.org/doi/10.1021/jacs.6b06288" target="_blank">25%</a>. Prior to my work, a halogen bond was described as only containing sigma symmetry. I showed the chemistry community for the first time that the bond also <a href="https://www.nature.com/articles/s41467-020-17122-7" target="_blank">contains pi covalency</a> with <a href="https://www.nature.com/articles/s41467-017-01726-7" target="_blank">non-conventional synchrotron techniques</a>.
+                I devised <a href="https://www.nature.com/articles/s41467-017-01726-7" target="_blank">a new way to characterize</a> the <a href="https://onlinelibrary.wiley.com/doi/abs/10.1002/anie.201510641" target="_blank">interfaces</a> within a solar cell. The electron transport at these interfaces determine the performance of the device, and my research improved the performance of solution-processed solar cells by <a href="https://pubs.acs.org/doi/10.1021/jacs.6b06288" target="_blank">25%</a>. My work also <a href="https://www.nature.com/articles/s41467-020-17122-7" target="_blank">redefined the halogen bond</a>—a bond previously misunderstood by the chemistry community.
             </p>
         </td>
     </tr>
 </table>
 
-<h3 class="experience-heading">I distributed an enclosure to protect healthcare workers from COVID-19<span class="heading-year"> | 2020</span></h3>
+<h3 class="experience-heading">I developed an enclosure to protect healthcare workers from COVID-19<span class="heading-year"> | 2020</span></h3>
 <table class="image-table">
     <tr>
         <td>
@@ -52,19 +52,19 @@
         </td>
         <td>
             <p>
-                Healthcare staff risk infection when intubating COVID-19 patients. I distributed an <a href="https://covidbox.ca/" target="_blank">open-source enclosure</a> to protect staff who are performing procedures on infectious patients. The enclosure was rapidly prototyped in <a href="https://www.youtube.com/watch?v=4mNC8dMF-p4">partnership with healthcare staff</a>, has been adopted in more than 5 countries, and is used on actual patients.
+                I developed an <a href="https://covidbox.ca/" target="_blank">open-source patient enclosure</a> to protect staff who are performing procedures on infectious patients. The enclosure was rapidly prototyped in <a href="https://www.youtube.com/watch?v=4mNC8dMF-p4">partnership with healthcare staff</a>, has been adopted in more than 5 countries, and is used on actual patients.
             </p>
         </td>
     </tr>
 </table>
 
 
-<h3 class="experience-heading">I taught chemistry lectures and laboratories at UBC and TWU<span class="heading-year"> | 2015-2016, 2020</span></h3>
+<h3 class="experience-heading">I taught first-year chemistry laboratories and lectures at UBC<span class="heading-year"> | 2015-2016, 2020</span></h3>
 <p>
     As a laboratory lecturer and teaching assistant, I Instructed >600 students and mentored 28 teaching assistants. I received >95% favorable <a href="http://demo.parlane.ca/documents/Fraser-Parlane-TA-evaluations.pdf" target="_blank">course reviews</a>.
 </p>
 
-<h3 class="experience-heading">I developed novel fluorine polymers for use as MRI contrast reagents<span class="heading-year"> | 2014</span></h3>
+<h3 class="experience-heading">I developed novel fluorine polymers for MRI contrast reagents<span class="heading-year"> | 2014</span></h3>
 <p>
     I built self-assembling block copolymers for use as MRI contrast reagents at ENSCM, France. I used RAFT polymerization to control the size distribution of fluorine- and phosphorus-containing polymers for use as tailored therapeutics.
 </p>
@@ -74,7 +74,7 @@
     I cofounded a tutoring company with 5 tutors that mentored secondary and post-secondary students in STEM subjects. Our services were always fully booked.
 </p>
 
-<h3 class="experience-heading">I built catalysts for pharmaceutical and polymer synthesis<span class="heading-year"> | 2010-2015</span></h3>
+<h3 class="experience-heading">I synthesized catalysts for pharmaceutical and polymer synthesis<span class="heading-year"> | 2010-2015</span></h3>
 <p>
-    I build <a href="https://www.sciencedirect.com/science/article/abs/pii/S002016931400019X" target="_blank">asymmetric catalysts</a> for advanced pharmaceutical synthesis and <a href="https://www.sciencedirect.com/science/article/abs/pii/S0020169317301937" target="_blank">polymerization</a>. I was awarded three NSERC research grants to perform this work.
+    I synthesized <a href="https://www.sciencedirect.com/science/article/abs/pii/S002016931400019X" target="_blank">asymmetric catalysts</a> for pharmaceutical and <a href="https://www.sciencedirect.com/science/article/abs/pii/S0020169317301937" target="_blank">polymer</a> applications. I was awarded three NSERC research grants to perform this work.
 </p>
