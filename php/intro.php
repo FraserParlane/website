0f6c870b34fc0cdd5751fde15569c52625a886fa
Fraser Parlane <mail@fraserparlane.com>
2022-09-22 10:20:54 -0700

    Thesis link updated.

--- php/intro.php
+++ php/intro.php
@@ -5,7 +5,7 @@
     <li><a href="php/mailto.php" class="button" target="_blank"><span class="icon solid fa-envelope"></span>Email</a></li>
     <li><a href="https://scholar.google.com/citations?user=iNFigAoAAAAJ&hl=en" class="button" target="_blank"><span class="icon solid fa-graduation-cap"></span>Google Scholar</a></li>
     <li><a href="https://github.com/FraserParlane" class="button" target="_blank"><span class="icon solid fa-code-branch"></span>GitHub</a></li>
-    <li><a href="http://www.defence.parlane.ca/" class="button" target="_blank"><span class="icon solid fa-book"></span>Thesis</a></li>
+    <li><a href="https://github.com/FraserParlane/thesis" class="button" target="_blank"><span class="icon solid fa-book"></span>Thesis</a></li>
 </ul>
 
 <p class="intro-text">
