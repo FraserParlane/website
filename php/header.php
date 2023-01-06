cc9439a52abfff1ccb93cc3429dad2be57cf2110
Fraser Parlane <mail@fraserparlane.com>
2023-01-06 13:36:31 -0800

    Formatting improvement.

--- php/header.php
+++ php/header.php
@@ -1,5 +1,5 @@
 <div class="inner">
-    <a href="#body" class="image avatar"><img src="images/avatar.webp" alt="" /></a>
+    <a href="#body" class="image avatar"><img src="images/avatar-grey.webp" alt="" /></a>
     <ul class="menu-links">
         <li><a href="#projects">Projects</a></li>
         <li><a href="#education">Education</a></li>
--- php/intro.php
+++ php/intro.php
@@ -1,12 +1,27 @@
 <span class="my-name">Fraser Parlane, PhD</span><br />
-<span class="my-name-slug">Data Scientist, Chemist, Project Manager</span>
+<span class="my-name-slug">
+    Data Scientist, Chemist, Project Manager<br />
+        <a href="php/mailto.php" target="_blank">Email</a>&nbsp;&nbsp;&nbsp;
+    <a href="https://scholar.google.com/citations?user=iNFigAoAAAAJ&hl=en" target="_blank">Google Scholar</a>&nbsp;&nbsp;&nbsp;
+    <a href="https://github.com/FraserParlane" target="_blank">GitHub</a>&nbsp;&nbsp;&nbsp;
+    <a href="https://github.com/FraserParlane/thesis" target="_blank">Thesis</a>
 
-<ul class="actions actions-top">
-    <li><a href="php/mailto.php" class="button" target="_blank"><span class="icon solid fa-envelope"></span>Email</a></li>
-    <li><a href="https://scholar.google.com/citations?user=iNFigAoAAAAJ&hl=en" class="button" target="_blank"><span class="icon solid fa-graduation-cap"></span>Google Scholar</a></li>
-    <li><a href="https://github.com/FraserParlane" class="button" target="_blank"><span class="icon solid fa-code-branch"></span>GitHub</a></li>
-    <li><a href="https://github.com/FraserParlane/thesis" class="button" target="_blank"><span class="icon solid fa-book"></span>Thesis</a></li>
-</ul>
+</span>
+
+
+<!--<div class="action-link-block">-->
+<!--    <a href="php/mailto.php" class="action-link" target="_blank">Email</a> |-->
+<!--    <a href="https://scholar.google.com/citations?user=iNFigAoAAAAJ&hl=en" class="action-link" target="_blank">Google Scholar</a> |-->
+<!--    <a href="https://github.com/FraserParlane" class="action-link" target="_blank">GitHub</a> |-->
+<!--    <a href="https://github.com/FraserParlane/thesis" class="action-link" target="_blank">Thesis</a>-->
+<!--</div>-->
+
+<!--<ul class="actions actions-top">-->
+<!--    <li><a href="php/mailto.php" class="button" target="_blank"><span class="icon solid fa-envelope"></span>Email</a></li>-->
+<!--    <li><a href="https://scholar.google.com/citations?user=iNFigAoAAAAJ&hl=en" class="button" target="_blank"><span class="icon solid fa-graduation-cap"></span>Google Scholar</a></li>-->
+<!--    <li><a href="https://github.com/FraserParlane" class="button" target="_blank"><span class="icon solid fa-code-branch"></span>GitHub</a></li>-->
+<!--    <li><a href="https://github.com/FraserParlane/thesis" class="button" target="_blank"><span class="icon solid fa-book"></span>Thesis</a></li>-->
+<!--</ul>-->
 
 <p class="intro-text">
     I earned my Ph. D. by leading an $8M project that successfully built the world’s first <a href="https://www.nature.com/articles/s41467-022-28580-6" target="_blank">autonomous thin-films robotic laboratory</a>. I have deployed complete scientific software stacks that include optical instrument design and integration, machine-learning models, data pipelines, cloud-based simulations, and interactive dashboards. I also <a href="https://github.com/calorimetry/CSID" target="_blank">processed</a> and visualized <a href="https://www.nature.com/articles/s41586-019-1256-6" target="_blank">nuclear fusion</a> reactor data at Google and <a href="https://www.nature.com/articles/s41467-017-01726-7" target="_blank">redefined a chemical bond</a> at the Stanford synchrotron during my doctorate. I am an expert in <a href="https://youtu.be/2h9-9PxwbQM" target="_blank">data-driven storytelling</a> and feature in <a href="https://www.nature.com/articles/s41586-019-1256-6" target="_blank">Nature</a>, <a href="https://www.nature.com/articles/s41578-021-00356-2" target="_blank">Nat. Rev. Mater.</a>, <a href="https://www.nature.com/articles/s41467-017-01726-7" target="_blank">Nat. Comm.</a> (<a href="https://www.nature.com/articles/s41467-022-28580-6" target="_blank">twice</a>), <a href="https://www.nature.com/articles/s41563-021-01156-3" target="_blank">Nat. Mater.</a>, <a href="https://www.nature.com/articles/nchem.2549" target="_blank">Nat. Chem.</a>, <a href="https://www.science.org/doi/full/10.1126/sciadv.aaz8867" target="_blank">Sci. Adv.</a>, and <a href="https://doi.org/10.1039/D2DD90013K" target="_blank">RSC (cover)</a>.
